e167f66ef3bab000ba69ae7e85ad929dd17c195c
a1m0ctane <amir.khan.offenbach.main@gmail.com>
2019-04-04 09:12:11 +0100

    user story #6102 - SCA Scan

--- src/main/java/eightball/EightBall.java
+++ src/main/java/eightball/EightBall.java
@@ -17,6 +17,20 @@ public class EightBall {
 
 		//Kommentar hinzugefügt code geschrieben
 	}
+	public static void mainer(String args[]) throws Exception {
+		char[] buffer = new char[1024];
+		String filename = args[0];
+		try {
+			filename = "" + (Integer.parseInt(filename) % 3);
+		} catch (Exception e) {
+			System.out.println("Invalid input.");
+		}
+		new FileReader(filename).read(buffer);
+		System.out.println(buffer);
+
+		//Kommentar hinzugefügt code geschrieben
+	}
+
 }
 
 
